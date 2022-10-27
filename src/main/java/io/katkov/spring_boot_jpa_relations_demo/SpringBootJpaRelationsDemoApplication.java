7ed1279ba3b96ceabc3240898a13c629657de2bd
redkatok <47156518+redkatok@users.noreply.github.com>
2022-10-27 21:16:11 +0300

    Initial commit

---
+++ src/main/java/io/katkov/spring_boot_jpa_relations_demo/SpringBootJpaRelationsDemoApplication.java
@@ -0,0 +1,14 @@
+package io.katkov.spring_boot_jpa_relations_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootJpaRelationsDemoApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringBootJpaRelationsDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/io/katkov/spring_boot_jpa_relations_demo/config/DomainConfig.java
@@ -0,0 +1,14 @@
+package io.katkov.spring_boot_jpa_relations_demo.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EntityScan("io.katkov.spring_boot_jpa_relations_demo.entity")
+@EnableJpaRepositories("io.katkov.spring_boot_jpa_relations_demo.domain.repository")
+@EnableTransactionManagement
+public class DomainConfig {
+}
